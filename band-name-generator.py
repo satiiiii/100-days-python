86f23a8b8bebcdef9225a5d5d395edc8552d8689
Seu Nome Completo <gabcunha733@gmail.com>
2026-08-08 16:47:29 -0300

    feat: add band generator project

---
+++ band-name-generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+city = input("Which city did you grow in?\n")
+name = input("What is the name of your pet?\n")
+print("Your band's name could be " + city + " " + name)
